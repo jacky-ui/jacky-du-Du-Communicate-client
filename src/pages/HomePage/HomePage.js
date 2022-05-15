740e406e0504cd2e1eca44f589174c28b8458145
jacky-ui <jacky.du.004@gmail.com>
2022-05-15 16:33:34 -0400

    installed library to decode jwt on front-end for requests

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,8 @@
+import axios from "axios";
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import Navigation from "../../components/Navigation/Navigation";
+import jwt_decode from "jwt-decode";
 import "./HomePage.scss";
 
 class HomePage extends Component {
@@ -11,12 +13,16 @@ class HomePage extends Component {
 
     componentDidMount() {
         const token = sessionStorage.getItem("token");
-        console.log(token);
 
         if (!token) {
             this.setState({ failedLogin: true });
             return;
         }
+
+        const decodedUser = jwt_decode(token);
+        const userId = decodedUser.id;
+        console.log(decodedUser);
+        console.log(userId);
     }
 
     render() {
