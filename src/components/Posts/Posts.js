4a405e56c53d4dd8b1f2a1413118803adb0f0426
jacky-ui <jacky.du.004@gmail.com>
2022-05-16 11:36:40 -0400

    create basic user posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,8 +2,18 @@ import "./Posts.scss";
 
 function Posts() {
     return (
-        <section>
-            <h1>Hello World</h1>
+        <section className="posts">
+            <div className="posts__profile">
+                <div className="posts__profile--sizing"></div>
+            </div>
+            <div className="posts__comments">
+                <div className="posts__comments--info">
+                    <span className="posts__comments--head">Username</span>
+                    <span className="posts__comments--head">02/03/2021</span>
+                </div>
+                <p className="posts__comments--comment">hfdkajhldfhasdhf fdsafhsdjkfhlaksjhfd fdsahfasldkf</p>
+                <div></div>
+            </div>
         </section>
     )
 }
