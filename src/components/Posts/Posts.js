b7a9ce53a745e1d242783721a0c8b41c34aa834f
jacky-ui <jacky.du.004@gmail.com>
2022-05-16 20:12:19 -0400

    basic dashboard set up

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,6 @@
 import "./Posts.scss";
+import likesIcon from "../../assets/images/icons/likes.svg";
+import commentIcon from "../../assets/images/icons/comment.png";
 
 function Posts() {
     return (
@@ -11,8 +13,19 @@ function Posts() {
                     <span className="posts__comments--head">Username</span>
                     <span className="posts__comments--head">02/03/2021</span>
                 </div>
-                <p className="posts__comments--comment">hfdkajhldfhasdhf fdsafhsdjkfhlaksjhfd fdsahfasldkf</p>
-                <div></div>
+                <p className="posts__comments--comment">hfdkajhldfhasdhf fdsafhsdjkfhlaksjhfd </p>
+                <div className="posts__icons">
+                    <img 
+                        src={likesIcon}
+                        className="posts__icons--sizing"
+                        alt="like user comment"
+                    />
+                    <img 
+                        src={commentIcon}
+                        className="posts__icons--sizing"
+                        alt="comment on user post"
+                    />
+                </div>
             </div>
         </section>
     )
