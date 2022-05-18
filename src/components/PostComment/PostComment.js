48a90f187a61dd2695da55f392ccc07911abe52a
jacky-ui <jacky.du.004@gmail.com>
2022-05-18 00:38:31 -0400

    create layout of profile, textarea and button

--- src/components/PostComment/PostComment.js
+++ src/components/PostComment/PostComment.js
@@ -7,11 +7,20 @@ class PostComment extends Component {
         console.log(this.props.profilePic)
         return (
         <Popup trigger={<input type="text" placeholder="Posts? Feelings?"></input>} modal nested>
-        {close => (
-            <div className="popup">
-                <h1>Hello world</h1>
-            </div>
-        )}
+            {close => (
+                <div className="popup">
+                    <h2 className="popup__title">Make a post!</h2>
+                    <div className="popup__comments">
+                        <img 
+                            className="popup__comments--profile" 
+                            src={this.props.profilePic} 
+                            alt="user profile"
+                        />
+                        <input className="popup__comments--comment" type="textarea"></input>
+                        <button>POST!</button>
+                    </div>
+                </div>
+            )}
         </Popup>
         )
     }
