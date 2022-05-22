9c508b1680564930909059b464f136cc2c45b14b
jacky-ui <jacky.du.004@gmail.com>
2022-05-22 14:45:35 -0400

    able to render and style message depend on sender

--- src/components/Chats/Messages.js
+++ src/components/Chats/Messages.js
@@ -1,13 +1,13 @@
 import { Component } from "react";
 import io from "socket.io-client";
 import sendIcon from "../../assets/images/icons/send.png";
+import uniqid from "uniqid";
 import "./Messages.scss";
 let socket = io.connect("http://localhost:8080");
 
 class Messages extends Component {
     state = {
         chat: [ ],
-        userMesaage: null,
         userCommented: [ ]
     }
 
@@ -35,27 +35,42 @@ class Messages extends Component {
             username: this.props.username
         });
 
-        this.setState ({ userMesaage: post })
+        // const userPosted = {
+        //     id: uniqid(),
+        //     username: this.props.username,
+        //     comments: post
+        // };
 
+        // this.setState ({ chat: [...this.state.chat, userPosted] });
         e.target.reset();
     }
 
+    compareUsername = (username) => {
+        if(this.props.username === username) {
+            return true;
+        }
+        return false;
+    }
+    
     render() {
         return (
             <>
             <section className="message">
                 <div className="message__container">
                     <h3 className="message__container--header">DU-CHAT</h3>
+                        <div className="message__container--overflow">
                             {this.state.chat.map((data) => {
                                 return(
-                                    <div className="message__container--receive" key={data.id}>
+                                    <div className={`${this.compareUsername(data.username) ? "message__container--posted" : "message__container--receive"}`} key={data.id}>
                                         <span className="message__container--username">{data.username}</span>
                                         <div>
-                                            <span className="message__container--comment">{data.comment}</span>
+                                            <span className={`${this.compareUsername(data.username) ? "message__container--commented" : "message__container--comment"}`}>{data.comment}</span>
                                         </div>
                                     </div>
                                 )
                             })}
+                        </div>
+
                 </div>
                     <form className="message__form" onSubmit={this.handleSend}>
                         <input className="message__form--input" type="text" placeholder="Write a Message..." name="post"/>
