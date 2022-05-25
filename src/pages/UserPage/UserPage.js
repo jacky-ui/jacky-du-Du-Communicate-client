5aec982e81cb6a74c61c9af3fab180e4e297db3d
jacky-ui <jacky.du.004@gmail.com>
2022-05-25 01:16:40 -0400

    create basic axios call that will console.log response

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,16 +1,52 @@
 import { Component } from "react";
+import FailedLogin from "../../components/FailedLogin/FailedLogin";
 import Navigation from "../../components/Navigation/Navigation";
 import Posts from "../../components/Posts/Posts";
 import SideNavigation from "../../components/SideNavigation/SideNavigation";
+import axios from "axios";
 import "./UserPage.scss";
 
 class UserPage extends Component {
+    state = {
+        failedLogin: false,
+        user: null
+    }
+
     componentDidMount = () => {
         const token = sessionStorage.getItem("token");
+        const userId = this.props.match.params.id;
+        console.log(this.props.match.params.id)
+       
+        if (!token) {
+            this.setState({ failedLogin: true });
+            return;
+        }
 
+        axios
+            .get(`http://localhost:8080/comments/${userId}`, {
+                headers: {
+                    Authorization: "Bearer" + token
+                }
+            })
+            .then((res) => {
+                console.log(res);
+            })
+    }
+
+    handleLogout = () => {
+        sessionStorage.removeItem("token");
+        this.setState({
+            user: null,
+            failedLogin: true
+        })
     }
 
     render() {
+        if (this.state.failedLogin) {
+            return(
+                <FailedLogin />
+            )
+        }
         return(
             <>
                 <Navigation />
@@ -22,7 +58,7 @@ class UserPage extends Component {
                             <span className="user__info--username">username</span>
                         </div>
                     </section>
-                    <SideNavigation />
+                    <SideNavigation handleLogout={this.handleLogout}/>
                 </main>
                 <h1 className="user__posts">POST HISTORY</h1>
                 <Posts />
