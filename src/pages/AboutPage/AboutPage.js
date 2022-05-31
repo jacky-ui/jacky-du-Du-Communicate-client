14609744c612be0ba0d8ffc40f83b4b55931e146
jacky-ui <jacky.du.004@gmail.com>
2022-05-31 15:08:50 -0400

    import nav and side nav. add welcome text

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,17 @@
 import "./AboutPage.scss";
+import Navigation from "../../components/Navigation/Navigation";
+import SideNavigation from "../../components/SideNavigation/SideNavigation";
 
 function AboutPage() {
     return (
         <>
-            <h1>Hello</h1>
+            <Navigation />
+            <article className="aboutme">
+                <section className="aboutme__container">
+                    <h1 className="aboutme__header">Welcome!<br/>You have found my About Me page!</h1>
+                </section>
+                <SideNavigation />
+            </article>
         </>
     )
 }
